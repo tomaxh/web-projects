47aa516c3d9f7c828771ef927a4c5925822bd0d3
tomaxh <qinxuyao@gmail.com>
2019-05-21 18:55:49 -0700

    added project clock

--- clock/index.js
+++ clock/index.js
@@ -1,1 +1,23 @@
 
+var clock = () => {
+    let tmp = new Date();
+    let options = [tmp.getHours(), tmp.getMinutes(), tmp.getSeconds()];
+
+    var helper = (time, position) => {
+        let clock = document.getElementsByTagName('img')[position];
+        clock.src = `./img/${Math.floor(time / 10)}.png`;
+
+        clock = document.getElementsByTagName('img')[position + 1];
+        clock.src = `./img/${Math.floor(time % 10)}.png`;
+
+    }
+
+    for (let i = 0; i < 5; i += 2) {
+        helper(options[i / 2], i)
+    }
+}
+
+window.onload = () => {
+    setInterval(clock, 1000);
+
+}
